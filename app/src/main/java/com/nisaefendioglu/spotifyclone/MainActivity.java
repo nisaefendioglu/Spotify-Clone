34949430919a7fcf29f50fd613c1c2c2957f23be
nisaefendioglu <nisa1517@gmail.com>
2021-08-31 18:01:29 +0300

    navbar added.

--- app/src/main/java/com/nisaefendioglu/spotifyclone/MainActivity.java
+++ app/src/main/java/com/nisaefendioglu/spotifyclone/MainActivity.java
@@ -3,15 +3,11 @@ package com.nisaefendioglu.spotifyclone;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView navView;
-    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
