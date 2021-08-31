497464f2fb0f635b77f9abb61408d1359159e644
nisaefendioglu <nisa1517@gmail.com>
2021-08-31 20:26:31 +0300

    search page added.

--- app/src/main/java/com/nisaefendioglu/spotifyclone/Search.java
+++ app/src/main/java/com/nisaefendioglu/spotifyclone/Search.java
@@ -4,19 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class Search extends Fragment {
 
     View view;
+    ConstraintLayout search_layout;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.search, container, false);
-
+        view = inflater.inflate(R.layout.search,container,false);
+        search_layout = view.findViewById(R.id.search_layout);
         return view;
+
     }
 }
